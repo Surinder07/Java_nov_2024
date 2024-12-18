f44ccfdf038667ff0e581cce94738e8730747c37
pragra <sunny@pragra.io>
2024-12-17 22:10:37 -0500

    arrays

---
+++ src/dec17/UserDemo.java
@@ -0,0 +1,23 @@
+package dec17;
+
+import java.util.Scanner;
+
+public class UserDemo {
+    public static void main(String[] args) {
+
+        demo("");
+    }
+
+    public static String demo(String input) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your input");
+
+        String input1 = scanner.next();
+
+        System.out.println(input1);
+
+        return "";
+    }
+
+}
