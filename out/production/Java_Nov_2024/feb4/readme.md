a879020fb7c982799cadc31a22ec11a4281b0b61
pragra <sunny@pragra.io>
2025-02-04 20:37:04 -0500

    comparable

---
+++ out/production/Java_Nov_2024/feb4/readme.md
@@ -0,0 +1,50 @@
+## Recap 
+
+1. Map interface 
+2. Set - TreeSet 
+
+HashSet vs LinkedHashSet vs TreeSet 
+
+Map ? 
+
+Map in an interface which stores data in key value pair. 
+where key has to be unique and value can be duplicated. 
+one null is allowed. 
+
+HashMap
+LinkedHashMap
+TreeMap 
+
+
+ConcurrentHashMap - HOME take assignment. 
+
+## Feb 4 
+1. Comparable Interface 
+2. Comparator 
+3. Collections
+4. Arrays
+5. Queue and Stack 
+
+
+## Comparable
+
+used for sorting 
+
+obj1 - this [current object] 
+obj2 - o    [other object ]
+
+
+obj1 < obj2 ---->  negative value
+obj1 = obj2 ----> 0 
+obj1 > obj2 ---->  positive value
+
+
+
+
+
+
+
+
+
+
+ 
---
+++ src/feb4/Test.java
@@ -0,0 +1,31 @@
+package feb4;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Mike", 23);
+        Employee e2 = new Employee("Peter", 54);
+        Employee e3 = new Employee("Emma", 14);
+        Employee e4 = new Employee("Mathew", 19);
+        Employee e5 = new Employee("John", 44);
+
+        List<Employee> list = new ArrayList<>();
+        list.add(e1);
+        list.add(e2);
+        list.add(e3);
+        list.add(e4);
+        list.add(e5);
+
+        Collections.sort(list);
+       // System.out.println(list);
+
+        for (Employee e : list) {
+            System.out.println(e.getName() + " :  "+e.getAge());
+        }
+    }
+}
